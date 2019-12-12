ed65db81a94a26424ba1fbd4763c3d194206d0f5
Karol Perec <karol.perec@gmail.com>
2019-12-12 22:33:44 +0100

    Add Lab1 init

---
+++ Lab1/Lab1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by Karol Perec on 12/12/2019.
+//  Copyright © 2019 Karol Perec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
