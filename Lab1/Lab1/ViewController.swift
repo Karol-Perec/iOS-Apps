1c3904e169d6d2646e9dc9082a902c9b09d33f93
Karol Perec <karol.perec@gmail.com>
2019-12-13 02:02:49 +0100

    Lab1 continue

--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var searchField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func search(_ sender: UIButton) {
+        debugPrint("KEKW")
+        Alamofire.request("https://aws.random.cat/meow")
+            .validate()
+            .responseJSON { response in
+                if let JSON = response.result.value {
+                    debugPrint("JSON: \(JSON)")
+                }
+        }
 }
-
+}
