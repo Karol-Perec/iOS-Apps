2f2de2c6e10ac87ff93f2c2e2e2c34b50d8fa2bd
Karol Perec <karol.perec@gmail.com>
2019-12-17 19:35:58 +0100

    in progress

---
+++ Lab1/Lab1/LongTermWeather.swift
@@ -0,0 +1,24 @@
+//
+//  LongTermWeather.swift
+//  Lab1
+//
+//  Created by Karol Perec on 17/12/2019.
+//  Copyright © 2019 Karol Perec. All rights reserved.
+//
+
+import UIKit
+
+class LongTermWeather: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Lab1/Lab1/ViewController.swift
+++ Lab1/Lab1/ViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var minTemperature: UILabel!
+    @IBOutlet weak var maxTemperature: UILabel!
+    @IBOutlet weak var weatherDescripiton: UILabel!
+    
+    let appId = "fb68fb31212518fa986c58366250f021"
+    let apiSearchUrl = "https://api.openweathermap.org/data/2.5/weather"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBOutlet weak var weather: UIStackView!
     @IBAction func search(_ sender: UIButton) {
         debugPrint("KEKW")
-        Alamofire.request("https://aws.random.cat/meow")
-            .validate()
-            .responseJSON { response in
-                if let JSON = response.result.value {
-                    debugPrint("JSON: \(JSON)")
-                }
-        }
+        
+        Alamofire.request(apiSearchUrl, parameters: ["q": searchField.text!, "APPID": appId, "units": "metric"])
+            .responseJSON { (response) in
+                let data = JSON(response.value!)
+                debugPrint(response.value!)
+                self.cityName.text = data["name"].stringValue
+                
+                let formatter = MeasurementFormatter()
+                formatter.numberFormatter.maximumFractionDigits = 0
+                
+                let temp = Measurement(value: data["main"]["temp"].doubleValue, unit: UnitTemperature.celsius)
+                self.temperature.text = formatter.string(from: temp)
+                
+                let minTemp = Measurement(value: data["main"]["temp_min"].doubleValue, unit: UnitTemperature.celsius)
+                self.minTemperature.text = formatter.string(from: minTemp)
+                
+                let maxTemp = Measurement(value: data["main"]["temp_max"].doubleValue, unit: UnitTemperature.celsius)
+                self.maxTemperature.text = formatter.string(from: maxTemp)
+                
+                let iconCode = data["weather"][0]["icon"].stringValue
+                Alamofire.request("https://openweathermap.org/img/wn/\(iconCode)@2x.png")
+                    .response(completionHandler: ({ (response) in
+                        self.weatherImage.image = UIImage(data: response.data!, scale: 1)
+                    }))
+                
+                self.weatherDescripiton.text = data["weather"][0]["description"].stringValue
+                
+                
+            }
 }
 }
